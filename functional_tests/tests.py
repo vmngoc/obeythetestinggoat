6c48bc5b16bf61242a530fbfcebd5b3139ebfb39
Demo User <demouser@n239-137.mtholyoke.edu>
2016-01-08 14:48:32 -0500

    start layout tests.

--- functional_tests/tests.py
+++ functional_tests/tests.py
@@ -71,7 +71,7 @@ class NewVisitorTest(LiveServerTestCase):
 
         #Francis visits the home page. There is no sign of Edith's list.
         self.browser.get(self.live_server_url)
-        page_text= self.browser.find_element_by_tag_name('body').text
+        page_text = self.browser.find_element_by_tag_name('body').text
         self.assertNotIn('Buy peacock feathers', page_text)
         self.assertNotIn('make a fly', page_text)
 
@@ -85,8 +85,28 @@ class NewVisitorTest(LiveServerTestCase):
         self.assertNotEqual(francis_list_url, edith_list_url)
 
         #There is still no trace of Edith's list
-        page_text= self.browser.find_element_by_tag_name('body').text
+        page_text = self.browser.find_element_by_tag_name('body').text
         self.assertNotIn('Buy peacock feathers', page_text)
         self.assertIn('Buy milk', page_text)
 
         # Satisfied, she goes back to sleep.
+
+    def test_layout_and_styling(self):
+        # She goes to check out its homepage.
+        self.browser.set_window_size(1024, 768)
+        self.browser.get(self.live_server_url)
+
+        # She notices that the input box is nicely centered
+        self.check_input_box_is_centered()
+
+        # She starts a new list
+        self.enter_a_new_item('testing')
+        self.check_input_box_is_centered()
+
+    def check_input_box_is_centered(self):
+        inputbox = self.browser.find_element_by_id('id_new_item')
+        self.assertAlmostEqual(
+            inputbox.location['x'] + (inputbox.size['width']/2),
+            512,
+            delta=5
+        )
